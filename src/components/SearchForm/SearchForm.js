213c81c50aa512a6cf16aae9bc1803576f0ccf10
Kay0s <hamilton.kristina@gmail.com>
2021-03-02 18:49:31 -0600

    updated handleChange on src/components/SearchForm/SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,18 +1,22 @@
 import React, {useState} from "react";
 import "./SearchForm.css";
 
- function SearchForm (props) {
-     const [employees, setEmployees] = useState({
-     orignalArr: props.employee
-     })
+ function SearchForm (props) { 
+     const [inputVal, setInputVal] = useState();
+     const [empFilter, setEmpFilter] = useState(props.employees);
+    
 
-     const handleChange = (e) => {
-         setEmployees({
-             firstName: e.target.value
-         })
-         console.log(employees.firstName);
-         employees.originalArr.filter(employee => employee.firstName.toLowerCase().indexOf(e.target.value) !== -1 );
-         this.setState({employees});
+     const handleChange = (e) => {  
+         setInputVal(
+            e.target.value
+         )
+         console.log(inputVal);
+         let lowerVal = e.target.value.toLowerCase();
+         let newArr = props.employees.filter(employee => employee.firstName.toLowerCase().indexOf(lowerVal) !== -1 );
+         console.log(props.employees);
+         console.log(newArr);
+         setEmpFilter(newArr);
+         console.log(newArr);
         } 
 
     return (
@@ -20,7 +24,6 @@ import "./SearchForm.css";
             <div className="form-group">
                 <label htmlFor="employee">Employee Name:</label>
                 <input
-                value={employees.firstName}
                 onChange={(e)=> handleChange(e)}
                 name="employee"
                 list="employees"
@@ -31,7 +34,7 @@ import "./SearchForm.css";
                
                 />
                 <datalist id="employees">
-                    {props.employees.map(id => (
+                    {empFilter.map(id => (
                         <option value={id} key={id} />
                     ))}
                     {/* filterEmployees={this.filterEmployees}
